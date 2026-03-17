2a21d6c4e03b4845cae7bf5d572c9e478cb90131
flagiu <inailuigoivalf@gmail.com>
2026-03-17 11:00:54 +0100

    1

--- push_and_deploy.sh
+++ push_and_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#git add . && git commit -m '1' && git push
-#gh workflow run "Resume Pipeline"
-#gh run list --workflow=pipeline.yaml
+make convert && \
+git add . && git commit -m '1' && git push && \
+gh workflow run "Resume Pipeline" && \
+gh run list --workflow=pipeline.yaml
